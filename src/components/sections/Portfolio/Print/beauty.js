b9f0140a25749fff99897be79d638070ea628f30
JuanPRubio <juanpablorubio94@gmail.com>
2022-03-16 04:39:20 -0400

    added Avon files to Beauty

--- src/components/sections/Portfolio/Print/beauty.js
+++ src/components/sections/Portfolio/Print/beauty.js
@@ -6,6 +6,8 @@ import img2 from "./Beauty/SM1.JPG";
 import img3 from "./Beauty/SM2.JPG";
 import img4 from "./Beauty/SP1.JPG";
 import img5 from "./Beauty/SP2.JPG";
+import img6 from "./Beauty/C02_SP_WN_Booked.JPG";
+import img7 from "./Beauty/C10-Marketing_Flyer_for_QC.JPG";
 
 //Routes
 import route1 from "./Beauty/espera.jpg";
@@ -13,6 +15,8 @@ import route2 from "./Beauty/SM1.pdf";
 import route3 from "./Beauty/SM2.pdf";
 import route4 from "./Beauty/SP1.pdf";
 import route5 from "./Beauty/SP2.pdf";
+import route6 from "./Beauty/C02_SP_WN_Booked.pdf";
+import route7 from "./Beauty/C10-Marketing_Flyer_for_QC.pdf";
 
 export const beauty = [
     {
@@ -49,6 +53,20 @@ export const beauty = [
       description: "Tips de belleza y cuidado personal.",
       image: img5,
       route: route5
+    },
+    {
+      hexa: "var(--first-color)",
+      title: "Qué hay de nuevo",
+      description: "Cuidado innovador para piel sensible.",
+      image: img6,
+      route: route6
+    },
+    {
+      hexa: "var(--first-color)",
+      title: "Suaviza y calma",
+      description: "Guía para darle el amor que tu piel necesita.",
+      image: img7,
+      route: route7
     }
-]
+  ]
   
